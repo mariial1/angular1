5706f0c239ee5a08961e6cbe1658a10479594cf6
Mariia Trusova <tmv_2008@ukr.net>
2021-01-24 17:47:59 +0200

    +

---
+++ src/db/data.ts
@@ -0,0 +1,11 @@
+ export let users = [
+  {name: 'olya', age: 25, status: false},
+  {name: 'valya', age: 31, status: true},
+  {name: 'ivan', age: 26, status: true},
+  {name: 'alex', age: 32, status: true},
+  {name: 'max', age: 33, status: false},
+  {name: 'mariia', age: 30, status: true},
+  {name: 'sveta', age: 29, status: false},
+  {name: 'petro', age: 30, status: true},
+  {name: 'oleg', age: 32, status: true}
+];
